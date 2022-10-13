03b6e5272b9bd179018091bef8a3f3cbb900c4f2
Lais <laismassacesi@gmail.com>
2022-10-13 14:03:42 -0300

    Requisito 1

---
+++ src/services/fsUtils.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+const TALKER_DB = '../talker.json';
+
+const readFile = async () => {
+  try {
+    const pathResolve = path.resolve(__dirname, TALKER_DB);
+    const fileResult = JSON.parse(await fs.readFile(pathResolve));
+    return fileResult; 
+  } catch (error) {
+    console.log(`error reading file: ${error}`);
+    return [];
+  }
+};
+
+readFile();
+
+module.exports = {
+  readFile,
+};
+
+// a
