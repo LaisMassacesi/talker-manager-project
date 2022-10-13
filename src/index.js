3c6e256618c0eeb0d96e398fd120419976ce5645
Lais <laismassacesi@gmail.com>
2022-10-13 17:58:18 -0300

    Requisito 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const fsUtils = require('./services/fsUtils');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
@@ -21,4 +22,19 @@ app.get('/talker', async (req, res) => {
   const talkers = await fsUtils.readFile();
 
   res.status(200).json(talkers);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const talkers = await fsUtils.readFile();
+  const { id } = req.params;
+  const message = {
+    message: 'Pessoa palestrante não encontrada',
+  };
+
+  if (id <= talkers.length) {
+    const talker = await talkers.find((obj) => obj.id === Number(id));
+  
+    return res.status(200).json(talker);
+  }
+  return res.status(404).json(message);
 });
